bb3bfad8c7f68d19fc5e22bcd1b5eb55a6ff2455
ranny80 <rannyvuong80@gmail.com>
2022-01-17 14:22:43 -0500

    Created main.py file

---
+++ main.py
@@ -0,0 +1,47 @@
+import turtle
+
+canvas = turtle.Screen()
+snowman = turtle.Turtle()
+
+# Background color
+canvas.bgcolor("light blue")
+
+canvas.title("Snowman Graphics")
+snowman.shape("circle")
+
+snowman.color("white")
+
+print("Building...")
+
+snowman.right(180)
+
+snowman.begin_fill()
+
+snowman.circle(85)
+
+snowman.end_fill()
+
+snowman.right(900)
+
+snowman.begin_fill()
+
+snowman.circle(65)
+
+snowman.end_fill()
+
+snowman.right(180)
+
+snowman.goto(0, 210)
+
+snowman.begin_fill()
+
+snowman.circle(40)
+
+snowman.end_fill()
+
+# It will disappear the turtle when the process is finished
+snowman.hideturtle()
+
+print("Build successful.")
+
+canvas.mainloop()
